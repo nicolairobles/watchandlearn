f911b76be04d5e4215bc369221235303e1de8f76
Nicolai Robles <nicolai.robles@gmail.com>
2016-02-11 16:59:49 -0500

    create rating and start filtering

--- spec/factories/votes.rb
+++ spec/factories/votes.rb
@@ -2,7 +2,7 @@ val = [-1,0,1]
 
 FactoryGirl.define do
   factory :vote do
-    value { val.sample }
+    value { 1 }
     user_id { rand(1..10) }
     video_id { rand(1..40) }
     curriculum_id { rand(1..10) }
