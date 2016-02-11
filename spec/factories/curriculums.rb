4a779ad26ed36e6c3758140984bb744aabc685d7
Marina Rozova <marinar578@gmail.com>
2016-02-11 12:20:38 -0500

    fix seed data

--- spec/factories/curriculums.rb
+++ spec/factories/curriculums.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
     title { Faker::Hacker.noun }
     description { Faker::Hipster.sentence }
     level { rand(1..3) }
-    user { rand(1..10) }
-    topic { rand(1..10) }
+    user_id { rand(1..10) }
+    topic_id { rand(1..10) }
   end
 end
