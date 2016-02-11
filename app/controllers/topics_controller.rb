e6c21e99be3f3dcbf92b276da2152add18b3b1b6
Marina Rozova <marinar578@gmail.com>
2016-02-11 16:20:27 -0500

    add curriculums by topic view

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -10,7 +10,7 @@ class TopicsController < ApplicationController
   # GET /topics/1
   # GET /topics/1.json
   def show
-      @curriculums = Curriculum.find_by(topic_id:params["id"])
+    @curriculums = Curriculum.where(topic_id:params["id"])
   end
 
   # GET /topics/new
