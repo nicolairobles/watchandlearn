4a350c0be85f915652a8ee272bff682a09cc590e
Nicolai Robles <nicolai.robles@gmail.com>
2016-02-14 15:29:42 -0500

    schema edit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160213165049) do
+ActiveRecord::Schema.define(version: 20160214190407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20160213165049) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "rating"
+    t.integer  "videocount"
   end
 
   add_index "curriculums", ["topic_id"], name: "index_curriculums_on_topic_id", using: :btree
