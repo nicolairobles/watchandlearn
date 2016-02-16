7bc1dddd9d1186411bdba69956c6a921ed450516
fatima faloye <f_faloye@verizon.net>
2016-02-16 16:13:13 -0500

    add gravatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+
   
 
 end
